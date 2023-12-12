572c346157a429298934f5b71995050cf1baece3
Sanidhaya Sharma <sanidhyas141@gmail.com>
2023-12-12 19:59:07 +0530

    next commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:alemeno_health_checkup/data/test_packages.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'data/health_test.dart';
@@ -18,7 +19,10 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Health Checkup"),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.badge))],
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.shopping_bag_outlined))
+        ],
       ),
       body: ResponsiveBuilder(builder: (context, sizingInformation) {
         return SingleChildScrollView(
@@ -34,7 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       .copyWith(fontWeight: FontWeight.bold),
                 ),
                 HealthTests(sizingInformation: sizingInformation),
-                Divider(color: Colors.black),
+                const Divider(color: Colors.black),
                 Text(
                   "Popular Packages",
                   style: Theme.of(context)
@@ -42,7 +46,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       .bodyLarge!
                       .copyWith(fontWeight: FontWeight.bold),
                 ),
-                HealthTests(sizingInformation: sizingInformation),
+                Center(child: PackageTest(package: healthPackages[0])),
               ]),
         );
       }),
@@ -50,126 +54,132 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class HealthTests extends StatefulWidget {
+// ignore: must_be_immutable
+class HealthTests extends StatelessWidget {
   HealthTests({super.key, required this.sizingInformation});
   SizingInformation sizingInformation;
 
-  @override
-  State<HealthTests> createState() => _HeathTestsState();
-}
-
-class _HeathTestsState extends State<HealthTests> {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
       shrinkWrap: true,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: widget.sizingInformation.isDesktop ? 4 : 2,
-          childAspectRatio: widget.sizingInformation.isDesktop ? 1.8 : 1.2),
-      itemCount: 4,
+          crossAxisCount: sizingInformation.isDesktop ? 4 : 2,
+          childAspectRatio: sizingInformation.isDesktop ? 1.8 : 1.2),
+      itemCount: sizingInformation.isDesktop ? 8 : 4,
       itemBuilder: (context, index) {
-        return Card(
-          elevation: 3,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 10,
+                offset: const Offset(0, 3),
+              ),
+            ],
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(3),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primary,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(10.0),
-                    topRight: Radius.circular(10.0),
+          child: Card(
+            elevation: 3,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10.0),
+                      topRight: Radius.circular(10.0),
+                    ),
+                  ),
+                  child: Text(
+                    tests[index].testName,
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelLarge!
+                        .copyWith(color: Colors.white, fontSize: 12),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
                   ),
                 ),
-                child: Text(
-                  tests[index].testName,
-                  style: Theme.of(context)
-                      .textTheme
-                      .labelLarge!
-                      .copyWith(color: Colors.white, fontSize: 12),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
+                Padding(
+                  padding: const EdgeInsets.all(3),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "Includes ${tests[index].includesHowManyTests} tests",
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(fontSize: 10),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 3),
+                        decoration: BoxDecoration(
+                          color: tests[index].isSafe
+                              ? Colors.greenAccent
+                              : Colors.redAccent,
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        child: Icon(
+                          tests[index].isSafe
+                              ? Icons.check_circle_outline
+                              : Icons.cancel_outlined,
+                          color: Theme.of(context).colorScheme.onPrimary,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(3),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        "Includes ${tests[index].includesHowManyTests} tests",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyMedium!
-                            .copyWith(fontSize: 10),
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 3),
-                      decoration: BoxDecoration(
-                        color: tests[index].isSafe
-                            ? Colors.greenAccent
-                            : Colors.redAccent,
-                        borderRadius: BorderRadius.circular(3),
-                      ),
-                      child: Icon(
-                        tests[index].isSafe
-                            ? Icons.check_circle_outline
-                            : Icons.cancel_outlined,
-                        color: Theme.of(context).colorScheme.onPrimary,
-                      ),
-                    )
-                  ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 3),
+                  child: Text(
+                    "Get reports in 24 hours",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodySmall!
+                        .copyWith(fontSize: 10),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 3),
-                child: Text(
-                  "Get reports in 24 hours",
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodySmall!
-                      .copyWith(fontSize: 10),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              // if(sizingInformation.isDesktop)
-
-              Padding(
-                padding: const EdgeInsets.all(3),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '₹${tests[index].priceInRupees}',
-                      style: const TextStyle(
-                        decoration: TextDecoration.lineThrough,
-                        color: Colors.red,
-                        fontSize: 10,
+                Padding(
+                  padding: const EdgeInsets.all(1),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '₹${tests[index].priceInRupees}',
+                        style: const TextStyle(
+                          decoration: TextDecoration.lineThrough,
+                          color: Colors.red,
+                          fontSize: 14,
+                        ),
                       ),
-                    ),
-                    Text(
-                      '₹${tests[index].discountPrice}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green,
-                        fontSize: 18,
+                      Text(
+                        '₹${tests[index].discountPrice}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                          fontSize: 18,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(3),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: ElevatedButton(
+                if (sizingInformation.isDesktop)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
                         onPressed: () {},
                         style: ElevatedButton.styleFrom(
                           backgroundColor:
@@ -183,16 +193,10 @@ class _HeathTestsState extends State<HealthTests> {
                               .copyWith(color: Colors.white, fontSize: 8),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(3),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: ElevatedButton(
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.01,
+                      ),
+                      ElevatedButton(
                         onPressed: () {},
                         style: ElevatedButton.styleFrom(
                           backgroundColor:
@@ -206,14 +210,153 @@ class _HeathTestsState extends State<HealthTests> {
                               .copyWith(color: Colors.white, fontSize: 8),
                         ),
                       ),
+                    ],
+                  ),
+                if (!sizingInformation.isDesktop)
+                  Padding(
+                    padding: const EdgeInsets.all(3),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.secondary,
+                            ),
+                            child: Text(
+                              "Add to Cart",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(color: Colors.white, fontSize: 8),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-            ],
+                  ),
+                if (!sizingInformation.isDesktop)
+                  Padding(
+                    padding: const EdgeInsets.all(3),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.secondary,
+                            ),
+                            child: Text(
+                              "View Details",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(color: Colors.white, fontSize: 8),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
           ),
         );
       },
+    );
+  }
+}
+
+class PackageTest extends StatelessWidget {
+  PackageTest({Key? key, required this.package}) : super(key: key);
+  final HealthPackage package;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.75,
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 10,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Card(
+        elevation: 0,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    package.icon,
+                    size: 40,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          package.title,
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Icon(
+                    package.isSafe ? Icons.check_circle : Icons.cancel,
+                    color: package.isSafe ? Colors.green : Colors.red,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Text(
+                "Includes ${package.includesHowManyTests} tests",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "₹${package.price}",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                    ),
+                    child: Text(
+                      "Add to Cart",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodySmall!
+                          .copyWith(color: Colors.white),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
