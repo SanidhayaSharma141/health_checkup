34aa31e8ce6ffa1251a35b1b4296a956957b9273
Sanidhaya Sharma <sanidhyas141@gmail.com>
2023-12-13 11:31:07 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
